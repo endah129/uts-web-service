8b37ec9dd4711aa07645e81541c517084a63bc15
Riki Sanjaya <imaderikiwidiastanasanjaya@gmail.com>
2025-11-04 11:39:49 +0800

    add layout dan default route ke home.blade.php; add about dan home page awalan

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('title', 'Home - UTS Web Service')
+
+@section('content')
+    <div style="padding: 2rem; text-align: center;">
+        <h1 style="color: white; font-size: 2.5rem; margin-bottom: 1rem; text-shadow: 2px 2px 4px rgba(0,0,0,0.5);">
+            Halaman ini dibuat oleh 2301010051 - I MADE RIKI WIDIASTANA SANJAYA (GANJIL)
+        </h1>
+    </div>
+@endsection
